5960f1f9ecf3bd799fd61d7d3564ea432d628148
Shusnuke Oba <obashun22@yahoo.co.jp>
2024-06-04 05:50:24 +0900

    Update MUI追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+
 import "./globals.css";
 
 const font = Roboto({ weight: "500", subsets: ["latin"] });
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
