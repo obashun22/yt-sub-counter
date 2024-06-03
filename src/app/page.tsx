ed8567bada5c5772bd30487a4ca042ca97e332cb
Shusnuke Oba <obashun22@yahoo.co.jp>
2024-06-04 05:21:22 +0900

    Update 表示画面作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="">hoge</main>;
+  return (
+    <main className="text-center">
+      <Image
+        src="/vercel.svg"
+        alt="logo"
+        width={120}
+        height={120}
+        className="mx-auto rounded-full w-[120px] h-[120px]"
+      />
+      <h1 className="text-xl font-bold">HikakinTV</h1>
+      <p className="my-6 text-5xl font-bold">1</p>
+      <p className="text-2xl font-bold">Subscribers</p>
+    </main>
+  );
 }
